54c29f593fd9db0b34f7fc2c90b0c532dd80e1db
LucasCta <lucas.ct.araujo@gmail.com>
2023-04-22 20:39:13 -0300

    Adding 1 submission to Week 4

---
+++ semana4/anagramas.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#define modulo 1000000007
+using namespace std;
+
+int fat[(5000 * 26) + 1];
+
+int mMod(int a, int b) {
+  unsigned long long la = a, lb = b;
+  return (la * lb) % modulo;
+}
+
+int fatorial(int n) {
+  if (n == 1 || n == 0)
+    fat[n] = 1;
+  if (fat[n] == -1)
+    fat[n] = mMod(n, fatorial(n - 1));
+  return fat[n];
+}
+
+int MDCE(int a, int b, int *x, int *y) {
+  if (b == 0) {
+    *x = 1;
+    *y = 0;
+    return a;
+  } else {
+    int *xt = (int *)malloc(sizeof(int));
+    int *yt = (int *)malloc(sizeof(int));
+    int d = MDCE(b, a % b, xt, yt);
+    *x = *yt;
+    *y = *xt - ((a / b) * (*yt));
+    return d;
+  }
+}
+
+int invMod(int b) {
+  int *x = (int *)malloc(sizeof(int));
+  int *y = (int *)malloc(sizeof(int));
+  MDCE(b, modulo, x, y);
+  return (*x + modulo) % modulo;
+}
+
+int main() {
+
+  for (int i = 0; i <= 5000 * 26; i++)
+    fat[i] = -1;
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int k;
+    cin >> k;
+    int *a = (int *)malloc(sizeof(int) * k);
+    int n = 0, N, Q = 1, iQ;
+    for (int i = 0; i < k; i++) {
+      cin >> a[i];
+      n += a[i];
+      Q = mMod(Q, fatorial(a[i]));
+    }
+    N = fatorial(n);
+    iQ = invMod(Q);
+    cout << mMod(N, iQ) << '\n';
+    free(a);
+  }
+
+  return 0;
+}
