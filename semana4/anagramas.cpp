38990d5c9883dd4b127994f692eecc605aa264c2
LucasCta <lucas.ct.araujo@gmail.com>
2023-04-23 23:01:28 -0300

    Correct submission of 'anagramas'

--- semana4/anagramas.cpp
+++ semana4/anagramas.cpp
@@ -2,7 +2,7 @@
 #define modulo 1000000007
 using namespace std;
 
-int fat[(5000 * 26) + 1];
+int fat[(50000 * 26) + 1];
 
 int mMod(int a, int b) {
   unsigned long long la = a, lb = b;
@@ -17,32 +17,39 @@ int fatorial(int n) {
   return fat[n];
 }
 
-int MDCE(int a, int b, int *x, int *y) {
+int MDCE(int a, int b, unsigned long long int *x, unsigned long long int *y) {
   if (b == 0) {
     *x = 1;
     *y = 0;
     return a;
   } else {
-    int *xt = (int *)malloc(sizeof(int));
-    int *yt = (int *)malloc(sizeof(int));
+    unsigned long long int *xt, *yt;
+    xt = (unsigned long long int *)malloc(sizeof(unsigned long long int));
+    yt = (unsigned long long int *)malloc(sizeof(unsigned long long int));
     int d = MDCE(b, a % b, xt, yt);
     *x = *yt;
     *y = *xt - ((a / b) * (*yt));
+    free(yt);
+    free(xt);
     return d;
   }
 }
 
 int invMod(int b) {
-  int *x = (int *)malloc(sizeof(int));
-  int *y = (int *)malloc(sizeof(int));
+  unsigned long long int *x, *y;
+  x = (unsigned long long int *)malloc(sizeof(unsigned long long int));
+  y = (unsigned long long int *)malloc(sizeof(unsigned long long int));
   MDCE(b, modulo, x, y);
   return (*x + modulo) % modulo;
 }
 
 int main() {
 
-  for (int i = 0; i <= 5000 * 26; i++)
+  for (int i = 0; i <= 50000 * 26; i++)
     fat[i] = -1;
+
+  for (int i = 0; i <= 50000 * 26; i += 1000)
+    fatorial(i);
 
   int t;
   cin >> t;
@@ -58,7 +65,7 @@ int main() {
     }
     N = fatorial(n);
     iQ = invMod(Q);
-    cout << mMod(N, iQ) << '\n';
+    cout << mMod(N, iQ) << "\n";
     free(a);
   }
 
