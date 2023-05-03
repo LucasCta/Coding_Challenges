946cdaca4deaaf0f49aa402ebc804801cf47dfaf
LucasCta <lucas.ct.araujo@gmail.com>
2023-05-03 03:10:10 -0300

    Adding 1 submission to Week 5

---
+++ semana5/formafechada.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    long int a, b, c;
+    cin >> a >> b >> c;
+    double f0, f1, f2;
+    f0 = c;
+    f1 = f0 + a + b;
+    f2 = f1 + a * 2 + b;
+    double x, y, z;
+    z = c;
+    x = (f2 - (2 * f1) + f0) / 2;
+    y = f1 - x - z;
+    printf("%g %g %g\n", x, y, z);
+  }
+  return 0;
+}
