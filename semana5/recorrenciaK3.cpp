4ca037690b36bbf1401a48962befa4f1b9406515
LucasCta <lucas.ct.araujo@gmail.com>
2023-05-03 00:05:03 -0300

    Adding 1 submission to Week 5

--- semana5/recorrenciaK3.cpp
+++ semana5/recorrenciaK3.cpp
@@ -2,20 +2,20 @@
 #define modulo 1000000007
 using namespace std;
 
-int multMod(int a, int b) {
+int multMod(long int a, long int b) {
   unsigned long long int la = a, lb = b;
   return (la * lb) % modulo;
 }
 
-int somaMod(int a, int b) {
+int somaMod(long int a, long int b) {
   unsigned long long int la = a, lb = b;
-  return (a + b) % modulo;
+  return (la + lb) % modulo;
 }
 
-int **multMat(int **A, int **B) {
-  int **mat = (int **)malloc(sizeof(int *) * 3);
+long int **multMat(long int **A, long int **B) {
+  long int **mat = (long int **)malloc(sizeof(long int *) * 3);
   for (int i = 0; i < 3; i++)
-    mat[i] = (int *)calloc(3, sizeof(int));
+    mat[i] = (long int *)calloc(3, sizeof(long int));
   for (int i = 0; i < 3; i++)
     for (int j = 0; j < 3; j++)
       for (int z = 0; z < 3; z++)
@@ -23,36 +23,37 @@ int **multMat(int **A, int **B) {
   return mat;
 }
 
-int **expMat(int **M, int n) {
-  if (n == 0) {
-    int **mat = (int **)malloc(sizeof(int *) * 3);
+long int **expMat(long int **M, int n) {
+  if (n <= 0) {
+    long int **mat = (long int **)malloc(sizeof(long int *) * 3);
     for (int i = 0; i < 3; i++) {
-      mat[i] = (int *)calloc(3, sizeof(int));
+      mat[i] = (long int *)calloc(3, sizeof(long int));
       mat[i][i] = 1;
     }
     return mat;
   }
-  if (n % 2 != 0)
-    return multMat(expMat(M, n - 1), M);
-  else {
-    int **X = expMat(M, n / 2);
-    return multMat(X, X);
+  if (n % 2 != 0) {
+    long int **e = expMat(M, n - 1);
+    long int **mlt = multMat(e, M);
+    for (int i = 0; i < 3; i++)
+      free(e[i]);
+    free(e);
+    return mlt;
+  } else {
+    long int **X = expMat(M, n / 2);
+    long int **mlt = multMat(X, X);
+    for (int i = 0; i < 3; i++)
+      free(X[i]);
+    free(X);
+    return mlt;
   }
 }
 
-int multMatVet(int **M, int *V) {
-  unsigned long long int acc = 0;
+int findTn(long int **M, long int *V) {
+  int acc = 0;
   for (int i = 0; i < 3; i++)
     acc = somaMod(acc, multMod(M[0][i], V[i]));
   return acc;
-}
-
-void printMat(int **M) {
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 3; j++)
-      cout << M[i][j];
-    cout << '\n';
-  }
 }
 
 int main() {
@@ -60,23 +61,27 @@ int main() {
   cin >> t;
   while (t--) {
 
-    int n;
+    long int n;
     cin >> n;
 
-    int **mat = (int **)malloc(sizeof(int *) * 3);
+    long int **mat = (long int **)malloc(sizeof(long int *) * 3);
     for (int i = 0; i < 3; i++) {
-      mat[i] = (int *)calloc(3, sizeof(int));
+      mat[i] = (long int *)calloc(3, sizeof(long int));
       cin >> mat[0][i];
       if (i > 0)
         mat[i][i - 1] = 1;
     }
 
-    int *t = (int *)malloc(sizeof(int) * 3);
+    long int *t = (long int *)malloc(sizeof(long int) * 3);
     for (int i = 2; i >= 0; i--)
       cin >> t[i];
 
-    cout << multMatVet(expMat(mat, n - 2), t) << '\n';
+    long int **e = expMat(mat, n - 2);
+    cout << findTn(e, t) << '\n';
 
+    for (int i = 0; i < 3; i++)
+      free(e[i]);
+    free(e);
     for (int i = 0; i < 3; i++)
       free(mat[i]);
     free(mat);
