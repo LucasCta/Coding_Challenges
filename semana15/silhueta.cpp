dac62acb6c018ceeef0708ac23acbbe14d0e8d3b
LucasCta <lucas.ct.araujo@gmail.com>
2023-08-17 21:16:33 -0300

    Adding 3 submissions

---
+++ semana15/silhueta.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define SIZE_VET 100 + 2
+
+typedef struct {
+  int x, y;
+} Ponto;
+
+typedef struct {
+  Ponto a, b;
+} Predio;
+
+int main() {
+  int t = 1;
+  while (t--) {
+    int N;
+    cin >> N;
+
+    int *vet = (int *)malloc(sizeof(int) * N);
+    Ponto *pontos = (Ponto *)malloc(sizeof(Ponto) * N + 1);
+  }
+  return 0;
+}
